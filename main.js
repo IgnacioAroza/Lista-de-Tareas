3e37c0a5cc0b334e9577e65f52f7106432bcd3f1
Ignacio Aroza <ignacioaroza.ia@gmail.com>
2023-04-11 14:27:45 -0300

    Se modifico el funcionamiento de js

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ const tituloInput = document.getElementById('titulo');
 const descripcionInput = document.getElementById('descripcion');
 const listaTareas = document.getElementById('tareas');
 
+const estado = "Pendiente";
+
+listaTareas.addEventListener("mouseup", function(event) {
+    const tareaId = event.target.getAttribute("data-id");
+    cambiarTarea(tareaId);
+});
+
 let tareas = [];
 
 function agregarTarea(e){
@@ -15,42 +22,107 @@ function agregarTarea(e){
     }
 
     const tarea = {
-        titulo : titulo,
-        descripcion : descripcion,
-        estado : 'Pendiente'
+        titulo,
+        descripcion,
+        estado
     }
 
     tareas.push(tarea);
     guardarTareas();
     mostrarTareas();
     form.reset;
+    // console.log(tareas);
 };
 
 function mostrarTareas() {
+    // console.log('Mostrando tareas...')
     listaTareas.innerHTML= '';
     tareas.forEach((tarea, index) => {
         const li = document.createElement('li');
-        li.innerHTML= `<span>${tarea.titulo}</span>
+        li.innerHTML= `
+        <span>${tarea.titulo}</span>
         <p>${tarea.descripcion}</p>
         <select data-id="${index}">
         <option value="pendiente"${tarea.estado === 'pendiente' ? ' selected' : ''}>Pendiente</option>
         <option value="enproceso"${tarea.estado === 'enproceso' ? ' selected' : ''}>En proceso</option>
         <option value="hecho"${tarea.estado === 'hecho' ? ' selected' : ''}>Hecho</option>
         </select>
+        <button data-id="${index}" class="editar">Editar</button>
         <button data-id="${index}" class="eliminar">Eliminar</button>
         `;
-        li.classList.add(tarea.estado);
+        li.classList.add(tarea.estado.toLowerCase());
         listaTareas.appendChild(li);
+    
     });
 }
 
-function cambiarEstado(e){
-    const select = e.target;
-    const id = select.dataset.id;
-    const estado = select.value;
-    tareas[id].estado = estado;
-    guardarTareas();
-    mostrarTareas();
+function agregarTareas(tareas, estado) {
+    if (tareas.length > 0) {
+    const estadoDiv = document.createElement('div');
+    estadoDiv.className = 'estado';
+    const estadoTitulo = document.createElement('h2');
+    estadoTitulo.textContent = estado;
+    estadoDiv.appendChild(estadoTitulo);
+    tareas.forEach(function(tarea, index) {
+        const tareaDiv = document.createElement('div');
+        tareaDiv.className = 'tarea';
+        const tareaTitulo = document.createElement('h3');
+        tareaTitulo.textContent = tarea.titulo;
+        const tareaDescripcion = document.createElement('p');
+        tareaDescripcion.textContent = tarea.descripcion;
+        const tareaEstado = document.createElement('select');
+        tareaEstado.innerHTML = `
+        <option value="pendiente" ${tarea.estado === 'pendiente' ? 'selected' : ''}>Pendiente</option>
+        <option value="en-proceso" ${tarea.estado === 'en-proceso' ? 'selected' : ''}>En proceso</option>
+        <option value="hecho" ${tarea.estado === 'hecho' ? 'selected' : ''}>Hecho</option>
+        `;
+        const tareaEditar = document.createElement('button');
+        tareaEditar.textContent = 'Editar';
+        tareaEditar.dataset.id = tareas.indexOf(tarea);
+        tareaEditar.className = 'editar';
+        tareaEditar.addEventListener('click', editarTareas);
+        const tareaEliminar = document.createElement('button');
+        tareaEliminar.textContent = 'Eliminar';
+        tareaEliminar.dataset.id = tareas.indexOf(tarea);
+        tareaEliminar.className = 'eliminar';
+        tareaEliminar.addEventListener('click', elimiarTarea);
+        tareaDiv.appendChild(tareaTitulo);
+        tareaDiv.appendChild(tareaDescripcion);
+        tareaDiv.appendChild(tareaEstado);
+        tareaDiv.appendChild(tareaEditar);
+        tareaDiv.appendChild(tareaEliminar);
+        estadoDiv.appendChild(tareaDiv);
+    });
+    listaTareas.appendChild(estadoDiv);
+    }
+}
+
+function cambiarTarea(id) {
+    let tarea = document.getElementById(id);
+    if (tarea) {
+    tarea.estado = !tarea.estado;
+    actualizarTarea(tarea);
+    } else {
+    // console.error("La tarea con ID " + id + " no se encontró.");
+    }
+}
+
+function editarTareas(e) {
+    if(e.target.classList.contains('editar')) {
+        const id = e.target.dataset.id;
+        const tarea = tareas[id];
+        tituloInput.value = tarea.titulo;
+        descripcionInput.value = tarea.descripcion;
+        form.removeEventListener('submit', agregarTarea);
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            tarea.titulo = tituloInput.value.trim();
+            tarea.descripcion = descripcionInput.value.trim();
+            guardarTareas();
+            mostrarTareas();
+            form.reset();
+        });
+    }
 }
 
 function elimiarTarea(e) {
@@ -64,6 +136,7 @@ function elimiarTarea(e) {
 
 function guardarTareas() {
     localStorage.setItem('tareas', JSON.stringify(tareas));
+    // console.log(localStorage.getItem('tareas'));
 }
 
 function cargarTareas() {
@@ -74,7 +147,26 @@ function cargarTareas() {
     mostrarTareas();
 }
 
+function crearSelect(opciones, valorSeleccionado, funcionCambio) {
+    const select = document.createElement('select');
+    select.addEventListener('change', funcionCambio);
+
+    for (const opcion of opciones) {
+    const option = document.createElement('option');
+    option.text = opcion;
+    option.value = opcion;
+    if (opcion === valorSeleccionado) {
+        option.selected = true;
+    }
+    select.appendChild(option);
+    }
+
+    return select;
+}
+
+
 form.addEventListener('submit', agregarTarea);
-listaTareas.addEventListener('change', cambiarEstado);
+listaTareas.addEventListener('change', cambiarTarea);
+listaTareas.addEventListener('click', editarTareas)
 listaTareas.addEventListener('click', elimiarTarea);
 cargarTareas();
